16e6c98cc5ead8710336aa54792fd0962232bd37
jason loring <jasonaloring@gmail.com>
2022-03-18 10:24:52 -0500

    Chirper Front end

---
+++ src/components/Main.jsx
@@ -0,0 +1,27 @@
+import React, { Component } from 'react';
+
+
+class Main extends Component {
+  render() {
+    return (
+      <React.Fragment>
+      
+      
+        <div id="mainDiv">
+          <div className="container">
+            <div className="row">
+              <div className="col-3">
+               
+                <User />
+              </div>
+            </div>
+          </div>
+        </div>
+
+      
+      </React.Fragment>
+    )
+  }
+}
+
+export default Main;
